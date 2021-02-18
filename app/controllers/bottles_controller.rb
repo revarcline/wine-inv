c7e85c3307ad8d51f5eedd26dcf9ea31a8c7edb1
Alex Cline <rev.a.r.cline@gmail.com>
2021-02-18 16:33:38 -0600

    new still kind of broken, coming back to this

--- app/controllers/bottles_controller.rb
+++ app/controllers/bottles_controller.rb
@@ -7,13 +7,36 @@ class BottlesController < ApplicationController
     set_bottle
   end
 
+  def new
+    @bottle = Bottle.new
+    all_params
+  end
+
+  def create
+    @bottle = Bottle.new(bottle_params(:name,
+                                       :vintage,
+                                       :producer_id,
+                                       :appellation_id,
+                                       :varietal_ids,
+                                       :desc))
+    @bottle.save
+    p @bottle
+    redirect_to bottle_path(@bottle)
+  end
+
   private
 
   def set_bottle
-    @bottle = Bottle.find(:id)
+    @bottle = Bottle.find(params[:id])
   end
 
   def bottle_params(*args)
     params.require(:bottle).permit(*args)
+  end
+
+  def all_params
+    @varietals = Varietal.all
+    @producers = Producer.all
+    @appellations = Appellation.all
   end
 end
