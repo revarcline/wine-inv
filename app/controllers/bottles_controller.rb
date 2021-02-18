731b6207522754b7cb14f4757b5401cc510bb08b
Alex Cline <rev.a.r.cline@gmail.com>
2021-02-17 21:33:37 -0600

    webpack fix, adding more controllers

--- app/controllers/bottles_controller.rb
+++ app/controllers/bottles_controller.rb
@@ -2,4 +2,18 @@ class BottlesController < ApplicationController
   def index
     @bottles = Bottle.all
   end
+
+  def show
+    set_bottle
+  end
+
+  private
+
+  def set_bottle
+    @bottle = Bottle.find(:id)
+  end
+
+  def bottle_params(*args)
+    params.require(:bottle).permit(*args)
+  end
 end
